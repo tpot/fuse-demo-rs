f9339d8a205bfd24c2932e6cbc6d18921537b3e1
Tim Potter <tpot@frungy.org>
2024-06-16 11:35:48 +1000

    Return . and .. directory entries

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # fuse-demo-rs
 
 Demo of using [FUSE-Rust](https://github.com/cberner/fuser) library crate to implement a FUSE filesystem.
+
+## Resources
+
+* [FUSE Protocol Documentation](https://web.archive.org/web/20070503133705/http://www.night.dataphone.se/~jrydberg/fuse.html) old, from Internet Archive
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,76 @@
+use std::time::{Duration, UNIX_EPOCH};
+use libc::ENOENT;
+
+const TTL: Duration = Duration::from_secs(1); // 1 second
+
+use fuser::{
+    FileAttr, FileType, Filesystem, MountOption,
+};
+
+struct DemoFS;
+
+const ROOT_DIR_ATTR: FileAttr = FileAttr {
+    ino: 1,
+    size: 0,
+    blocks: 0,
+    atime: UNIX_EPOCH, // 1970-01-01 00:00:00
+    mtime: UNIX_EPOCH,
+    ctime: UNIX_EPOCH,
+    crtime: UNIX_EPOCH,
+    kind: FileType::Directory,
+    perm: 0o755,
+    nlink: 2,
+    uid: 501,
+    gid: 20,
+    rdev: 0,
+    flags: 0,
+    blksize: 512,
+    padding: 0,
+};
+
+impl Filesystem for DemoFS {
+
+    fn getattr(&mut self, _req: &fuser::Request<'_>, ino: u64, reply: fuser::ReplyAttr) {
+        println!("getattr ino={ino}");
+        match ino {
+            1 => reply.attr(&TTL, &ROOT_DIR_ATTR),
+            _ => reply.error(ENOENT),
+        }
+    }
+
+    fn readdir(
+            &mut self,
+            _req: &fuser::Request<'_>,
+            ino: u64,
+            _fh: u64,
+            offset: i64,
+            mut reply: fuser::ReplyDirectory,
+        ) {
+        println!("readdir ino={ino}");
+
+        if ino != 1 {
+            reply.error(ENOENT);
+            return;
+        }
+
+        let entries = vec![
+            (1, FileType::Directory, "."),
+            (1, FileType::Directory, ".."),
+        ];
+
+        for (i, entry) in entries.into_iter().enumerate().skip(offset as usize) {
+            // i + 1 means the index of the next entry
+            if reply.add(entry.0, (i + 1) as i64, entry.1, entry.2) {
+                break;
+            }
+        }
+        reply.ok();
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mountpoint = "foo";
+    let mut options = vec![MountOption::RO, MountOption::FSName("foo".to_string())];
+    options.push(MountOption::AutoUnmount);
+    fuser::mount2(DemoFS, mountpoint, &options).unwrap();
 }
